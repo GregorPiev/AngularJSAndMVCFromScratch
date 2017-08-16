566d4f6a9e6ef523980a5ee4b8b9e0bae0caaa3c
Administrator <Administrator@DESKTOP-5OJ79TR>
2017-08-16 14:15:47 +0300

    Add project files.

---
+++ AngularJSAndMVCFromScratch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJSAndMVCFromScratch.Startup))]
+namespace AngularJSAndMVCFromScratch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
